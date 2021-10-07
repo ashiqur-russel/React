010022533216a539202e46b7a827999478b60263
ashiqurras <ashiqurrussel2@gmail.com>
2021-10-07 12:15:58 +0200

    making simple and reduce coding

--- test/src/index.js
+++ test/src/index.js
@@ -9,30 +9,17 @@ function BookList(){
   return (
     <section className='booklist'>
       <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
     </section>
   );
 }
 
 const Book = () => {
   return <article className='book'>
-    <Image />
-    <Title />
-    <Author />
+    <img className="cen" src="https://images-na.ssl-images-amazon.com/images/I/51MB5SD5GIL._AC_SX368_.jpg" alt="Check Image Link"></img>
+    <h1>Little Blue Truck's Halloween</h1>
+    <h4>Jill McElmurry</h4>
   </article>;
 }
 
-const Image =()=> <img className="cen" src="https://images-na.ssl-images-amazon.com/images/I/51MB5SD5GIL._AC_SX368_.jpg" alt="Check Image Link"></img>;
-const Title =() => <h1>Little Blue Truck's Halloween</h1>;
-const Author =() => <h4 style={{color:'#617d98',fontSize:'0.75rem', margin:'0.25rem'}}>Jill McElmurry</h4>;
-
-
 ReactDOM.render(<BookList/>, document.getElementById('root'));
+
