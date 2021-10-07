a1bb5828318ec2b731f4108f8d193bd7e38d2a94
ashiqurras <ashiqurrussel2@gmail.com>
2021-10-07 12:25:32 +0200

    set variables for making dynamic

--- test/src/index.js
+++ test/src/index.js
@@ -4,6 +4,12 @@ import ReactDOM from 'react-dom';
 //CSS
 import './index.css';
 
+//Set variable
+
+const title    = "Little Blue Truck's Halloween";
+const author  = "Jill McElmurry";
+const img= "https://images-na.ssl-images-amazon.com/images/I/51MB5SD5GIL._AC_SX368_.jpg";
+
 // Nested components, React Tools
 function BookList(){
   return (
@@ -13,11 +19,12 @@ function BookList(){
   );
 }
 
+
 const Book = () => {
   return <article className='book'>
-    <img className="cen" src="https://images-na.ssl-images-amazon.com/images/I/51MB5SD5GIL._AC_SX368_.jpg" alt="Check Image Link"></img>
-    <h1>Little Blue Truck's Halloween</h1>
-    <h4>Jill McElmurry</h4>
+    <img  src={img} alt=""></img>
+    <h1>{title}</h1>
+    <h4>{author}</h4>
   </article>;
 }
 
