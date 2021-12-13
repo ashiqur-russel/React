9a2b0c6e2ffeaa4972347bb6a7b34168b48d8e55
ashiqurras <ashiqurrussel2@gmail.com>
2021-12-13 02:24:02 +0100

    react basic fundamental

--- test/src/index.js
+++ test/src/index.js
@@ -1,49 +1,24 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDom from 'react-dom';
 
-
-//CSS
+// CSS
 import './index.css';
 
-//Set variable
+import {data} from './books'
+import SpecificBook from './Book'
 
-const firstBook = {
-  img : "https://images-na.ssl-images-amazon.com/images/I/A1mGYasTtES._AC_UL200_SR200,200_.jpg",
-  title : "The Bad Seed Presents: The Good, the Bad, and the Spooky",
-  author : "Pete Oswald"
-}
+function BookList() {
+  
 
-const secondBook = {
-  img : "https://images-na.ssl-images-amazon.com/images/I/41sWAELGloL._AC_UL200_SR200,200_.jpg",
-  title : "Room on the Broom",
-  author : "Julia Donaldson"
-}
-
-
-// Nested components, React Tools
-function BookList(){
   return (
     <section className='booklist'>
-      <Book img ={firstBook.img} title={firstBook.title} author={firstBook.author}> 
-      <p>Lorem ipsum dolor sit amet, aboris sint cupidatat ullamco ut ea consectetur et est culpa et
-      culpa duis.</p>
-      </Book>     
-      
-      <Book img ={secondBook.img} title={secondBook.title} author={secondBook.author}/>   
+      {data.map((book, index) => {
+        return (<SpecificBook key={book.id} {...book}></SpecificBook>)
+      })}
     </section>
   );
 }
 
-// Book component s
-const Book = ({img,title,author,children}) => {
-return ( 
-    <article className='book'>
-      <img  src={img} alt=""></img>
-      <h1>{title}</h1>
-      <h4>{author}</h4>
-      {children}
-    </article>);
-}
 
-ReactDOM.render(<BookList/>, document.getElementById('root'));
 
+ReactDom.render(<BookList />, document.getElementById('root'));
