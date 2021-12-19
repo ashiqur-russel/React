fd332924f49621a4d9589d50d4cfb62fba6bd6bc
ashiqurras <ashiqurrussel2@gmail.com>
2021-12-19 01:52:06 +0100

    navbar

--- icecream-builder/src/App.js
+++ icecream-builder/src/App.js
@@ -1,10 +1,16 @@
 import React from "react";
 import Products from "./components/products/Products";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <div className="App">
+    <div>
+      <Navbar />
+    </div>
+    <div>
       <Products />
+    </div>
     </div>
   );
 }
