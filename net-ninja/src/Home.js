2685a275db71f8e0ae8f6d7a8ffe86b92374d419
ashiqurras <ashiqurrussel2@gmail.com>
2022-01-16 19:15:58 +0100

    state with class component

--- net-ninja/src/Home.js
+++ net-ninja/src/Home.js
@@ -1,18 +1,28 @@
-const Home = () => {
-    const handleClick = () => {
-        console.log('Hello Ninjas');
+import React, { Component } from 'react'
+
+class Home extends Component {
+    // eslint-disable-next-line no-useless-constructor
+    constructor() {
+        super();
+        this.state = {
+            data: 1
+        }
     }
 
-    const handleClickAgain = (name, e) => {
-        console.log('Hello ' + name, e.target);
+    updateData() {
+        this.setState({ data: this.state.data + 1 })
+        console.log("hello")
     }
-    return (
-        <div className="home">
-            <h1>HOME</h1>
-            <button onClick={handleClick}>Click Me</button>
-            <button onClick={(e) => handleClickAgain('mario', e)}>Click Me Again</button>
-        </div>
-    );
+
+    render() {
+        return (
+            <div>
+                <h1>Home</h1>
+                <p>{this.state.data}</p>
+                <button onClick={() => this.updateData()}>Update Data</button>
+            </div>
+        )
+    }
 }
 
-export default Home;
+export default Home
